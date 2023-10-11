0c87811866d088ec94ad1df78ac26f5711b37ca2
Ilya Ivanov <ilyaivanov101@yandex.ru>
2023-10-11 18:30:42 +0300

    + Card Field in Payment

--- src/test/java/pages/PaymentPage.java
+++ src/test/java/pages/PaymentPage.java
@@ -1,16 +1,30 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import data.DataHelper;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class PaymentPage {
-    private final SelenideElement fieldCardNumber = ;
-    private final SelenideElement fieldMonth = ;
-    private final SelenideElement fieldYear = ;
-    private final SelenideElement fieldCardHolderName = ;
-    private final SelenideElement fieldCVC = ;
+    private final SelenideElement fieldCardNumber = $$(".input__control")
+            .find(exactText("Номер карты"));
+    private final SelenideElement fieldMonth = $$(".input__control")
+            .find(exactText("Месяц"));
+    private final SelenideElement fieldYear = $$(".input__control")
+            .find(exactText("Год"));
+    private final SelenideElement fieldCardHolderName = $$(".input__control")
+            .find(exactText("Владелец"));
+    private final SelenideElement fieldCVC = $$(".input__control")
+            .find(exactText("CVC/CVV"));
+
+    public void setCardInfo (DataHelper.CardInfo cardInfo) {
+        fieldCardNumber.setValue(cardInfo.getCardNumber());
+        fieldMonth.setValue(cardInfo.getCardMonth());
+        fieldYear.setValue(cardInfo.getCardYear());
+        fieldCardHolderName.setValue(cardInfo.getCardHolder());
+        fieldCVC.setValue(cardInfo.getCardCVV());
+    }
 
     private final SelenideElement buttonContinue = $$(".button__content")
             .find(exactText("Продолжить"));
@@ -52,5 +66,11 @@ public class PaymentPage {
             .find(exactText("Истёк срок действия карты"));
     public void messageCardHasExpired() {
         cardHasExpired.waitUntil(visible, 15000);
+    }
+
+    private final SelenideElement consistsOfLetters = $$(".input__sub")
+            .find(exactText("Должно состоять из букв"));
+    public void messageConsistsOfLetters() {
+        consistsOfLetters.waitUntil(visible, 15000);
     }
 }
